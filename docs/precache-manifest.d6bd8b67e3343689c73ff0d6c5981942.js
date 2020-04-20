cdfb502a7f57d8149581f4b490653efc3fe750c6
luciaherrrador <luciaherradorcarrasco@gmail.com>
2020-04-20 14:30:22 +0200

    add docs folder

---
+++ docs/precache-manifest.d6bd8b67e3343689c73ff0d6c5981942.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06f9dd6a6cbf3c791010ca406a070cc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "267d5b5f85ce6d734efb",
+    "url": "./static/css/main.11f479c8.chunk.css"
+  },
+  {
+    "revision": "ccb7eeb85695f6dc3df8",
+    "url": "./static/js/2.013e172d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.013e172d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "267d5b5f85ce6d734efb",
+    "url": "./static/js/main.1603135c.chunk.js"
+  },
+  {
+    "revision": "24c2a3a2774b1eb32f43",
+    "url": "./static/js/runtime-main.76913305.js"
+  },
+  {
+    "revision": "15022db57305b7b6291798e19a21ea42",
+    "url": "./static/media/burbujas.15022db5.jpg"
+  },
+  {
+    "revision": "f1df76866abf10e22ea018e407ef54e0",
+    "url": "./static/media/ico-menu.f1df7686.svg"
+  },
+  {
+    "revision": "3954f253f142a3d7ee22a4193fe6852a",
+    "url": "./static/media/me.3954f253.jpg"
+  },
+  {
+    "revision": "920a2c3ed5c236a6d104b1a7c5a6d4e1",
+    "url": "./static/media/web0.920a2c3e.png"
+  },
+  {
+    "revision": "2d23e1915441bb9380c752bcda9f30e7",
+    "url": "./static/media/web1.2d23e191.png"
+  },
+  {
+    "revision": "b448b67276c3675bf8453ec96866a889",
+    "url": "./static/media/web2.b448b672.png"
+  },
+  {
+    "revision": "2409593b01791767c7628e7629796619",
+    "url": "./static/media/web3.2409593b.png"
+  },
+  {
+    "revision": "646961eaaa8455a3cae1945e7b7a4ae7",
+    "url": "./static/media/web4.646961ea.png"
+  },
+  {
+    "revision": "85ba8afd6c8ceb0a71070b2fc9b61ef3",
+    "url": "./static/media/web5.85ba8afd.png"
+  },
+  {
+    "revision": "6631484af9ecaf30262d2d140fff9c65",
+    "url": "./static/media/web6.6631484a.png"
+  },
+  {
+    "revision": "e995c870b568c57bc59dbccf1b1962b9",
+    "url": "./static/media/web7.e995c870.png"
+  }
+]);
